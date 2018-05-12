728af7de884870330f3a6002fb7e2fbed9fcf67b
Kiel Dehnert <kielzor@gmail.com>
2018-05-12 16:43:09 -0600

    Added HTML CSS & JS

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+var inputTitle = document.querySelector(".input-title");
+var inputUrl = document.querySelector(".input.Url");
+var enterButton = document.querySelector(".enter-button");
+
+inputTitle.addEventListener('keyup', title);
+inputUrl.addEventListener('keyup', url);
+enterButton.addEventListener('click', enterButton);
+
