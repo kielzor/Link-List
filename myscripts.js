6c2c6e3aceccff03f98b21fb2225fc1355579a6d
Kiel Dehnert <kielzor@gmail.com>
2018-05-13 16:50:49 -0600

    Add Java to form HTML

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,26 @@
 var inputTitle = document.querySelector(".input-title");
-var inputUrl = document.querySelector(".input.Url");
-var enterButton = document.querySelector(".enter-button");
+var inputUrl = document.querySelector(".input-url");
+var submitButton = document.querySelector(".enter-button");
+var outputList = document.querySelector('.output-list');
 
-inputTitle.addEventListener('keyup', title);
-inputUrl.addEventListener('keyup', url);
-enterButton.addEventListener('click', enterButton);
+inputTitle.addEventListener('keyup', buttonEnable);
+inputUrl.addEventListener('keyup', buttonEnable);
+submitButton.addEventListener('click', makeBookmark);
 
+function makeBookmark(event) {
+    event.preventDefault();
+    outputList.innerHTML = `<form class="template"> 
+    <div class="output-title">${inputTitle.value}</div>
+    <div class="output-url">${inputUrl.value}</div>
+    <button class="read template-button">Read</button>
+    <button class="delete template-button">Delete</button>
+    </form>`;
+};
+
+function buttonEnable() {
+    if (inputTitle.value !== "" && inputUrl.value !== "") {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+};
