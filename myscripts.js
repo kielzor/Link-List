80ac313af2f50ee226f761f9af2f2bd6c60c1aae
Kiel Dehnert <kielzor@gmail.com>
2018-05-14 16:17:06 -0600

    Added css and bookmark fix

--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,48 @@
 var inputTitle = document.querySelector(".input-title");
 var inputUrl = document.querySelector(".input-url");
 var submitButton = document.querySelector(".enter-button");
-var outputList = document.querySelector('.output-list');
+var outputList = document.querySelector(".output-list");
+var counter = document.querySelector(".counter");
+var errorMessage = document.querySelector(".error-message");
+var unreadTotal = 0;
+var readTotal = 0;
 
-inputTitle.addEventListener('keyup', buttonEnable);
-inputUrl.addEventListener('keyup', buttonEnable);
-submitButton.addEventListener('click', makeBookmark);
+inputTitle.addEventListener("keyup", buttonEnable);
+inputUrl.addEventListener("keyup", buttonEnable);
+submitButton.addEventListener("click", makeBookmark);
 
-function makeBookmark(event) {
-    event.preventDefault();
-    outputList.innerHTML = `<form class="template"> 
-    <div class="output-title">${inputTitle.value}</div>
-    <div class="output-url">${inputUrl.value}</div>
-    <button class="read template-button">Read</button>
-    <button class="delete template-button">Delete</button>
-    </form>`;
-};
+function makeBookmark(i) {
+  event.preventDefault();
+  for (var i = 0; i < makeBookmark.length; i++) {
+    unreadTotal++;
+    var newCard = document.createElement("li");
+    newCard.innerHTML = `<div class="output-title">${inputTitle.value}</div>
+  <div class="output-url"><a class="output-hover" href='${inputUrl.value}'>${
+      inputUrl.value
+    }</a></div>
+    <button class="output-hover read-button template-button">Read</button>
+    <button class="output-hover delete-button template-button">Delete</button>
+    `;
+    outputList.appendChild(newCard);
+    deleteButton = document.querySelector(".delete-button");
+    deleteButton.addEventListener("click", deleteBookmark);
+    updateTotals();
+  }
+}
+
+function deleteBookmark(i) {
+  console.log("yay");
+}
 
 function buttonEnable() {
-    if (inputTitle.value !== "" && inputUrl.value !== "") {
-        submitButton.disabled = false;
-    } else {
-        submitButton.disabled = true;
-    }
-};
+  if (inputTitle.value !== "" && inputUrl.value !== "") {
+    submitButton.disabled = false;
+    errorMessage.innerText = "";
+  } else {
+    submitButton.disabled = true;
+  }
+}
+
+function updateTotals() {
+  counter.innerText = "Read: " + readTotal + " Unread: " + unreadTotal;
+}
